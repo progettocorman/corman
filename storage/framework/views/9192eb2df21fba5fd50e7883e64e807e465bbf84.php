8b7abc4063d1050bedff3504af11abb1f0a9c16b
progettocorman <christian.angelelli@live.it>
2018-01-31 21:53:53 +0100

    [Giovanni] Interfaccia pagina iniziale

--- storage/framework/views/9192eb2df21fba5fd50e7883e64e807e465bbf84.php
+++ storage/framework/views/9192eb2df21fba5fd50e7883e64e807e465bbf84.php
@@ -10,60 +10,55 @@
 <body>
 
 
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <!-- Menù di destra a tendina -->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="#">Corman</a>
-        <img src="corman.png">
       </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-        <!-- Cerca -->
-        <form class="navbar-form navbar-left">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search researcher">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+      <div class="collapse navbar-collapse" id="myNavbar">
+      </div>
+    </div>
   </nav>
 
 
+  <div class="container-fluid text-center">
+    <div class="row content">
+      <div class="col-sm-6 sidenav">
+  <form method="POST" action='login'>
 
-<form method="POST" action='login'>
-
-  <div id="grigio">
-   <form>
-     <div class="form-group_input">
-       <label for="exampleInputEmail1">Email address</label>
-       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-     </div>
-     <div class="form-group_input">
-       <label for="exampleInputPassword1">Password</label>
-       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-     </div>
-     <div class="form-check">
-       <input type="checkbox" class="form-check-input" id="exampleCheck1">
-       <label class="form-check-label" for="exampleCheck1">Check me out</label>
-     </div>
-     <button type="submit" class="btn btn-primary">Submit</button>
-   </form>
-
+  <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>">
+    <div class="form-group row">
+      <label for="imputEmail" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" name="user_email" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" name="user_password" placeholder="Password">
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+    <button type="button" onClick="location.href='formregister'">Signin</button>
+  </form>
   </div>
-</form>
+  <div class="col-sm-6 text-left">
+    <h1>Welcome</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <hr>
+    <h3>Test</h3>
+    <p>Lorem ipsum...</p>
+  </div>
 
-<button type="button" onClick="location.href='formregister'">Signin</button>
+</div>
+</div>
+
 
 
 
@@ -71,6 +66,10 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Stiamo includendo i javascript di boodstrap dalla nostra cartella -->
 <script src="js/bootstrap.min.js"></script>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
 
 
 </body>
