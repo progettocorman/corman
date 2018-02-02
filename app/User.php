232c76bea38ed9417e0434c74d3de4fe975b6944
Rick <kagebushin96@gmail.com>
2018-02-02 16:39:52 +0100

    [Rick]Aggiustato delle cose

--- app/User.php
+++ app/User.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-
-    protected $table = 'users';
+    use Searchable;
 
 }
--- app/UsersPost.php
+++ app/UsersPost.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersPost extends Model
 {
-    //
+  use Searchable;
 }
