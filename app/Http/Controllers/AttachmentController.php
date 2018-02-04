838c40bf788912795008299b3f2c151569a5021f
Enrico <progettocorman@gmail.com>
2018-02-04 12:51:06 +0100

    [Enrico]Pagina creazione post e pubblicazioni

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -9,21 +9,12 @@ use App\AttachmentPublication;
 
 class AttachmentController extends Controller
 {
-    public static function addAttachment($id_subject, $type_subject, $name_attachment, $type_file, $file_name){
+    public static function addAttachment($id_subject, $type_subject, $file){
       if($type_subject == 0){ //post
         $attachment = new AttachmentPost;
-        $attachment->posts_id = $id_subject;
-        $attachment->namefile = "$type_subject-$id_subject-$name_attachment";
-        $attachment->typefile= $type_file;
-        $attachment-> save();
       }
       else { //publication
         $attachment = new AttachmentPublication;
-        $attachment->posts_id = $id_subject;
-        $attachment->namefile = "$type_subject-$id_subject-$name_attachment";
-        $attachment->typefile= $type_file;
-        $attachment-> save();
-      }
 
     }
 }
