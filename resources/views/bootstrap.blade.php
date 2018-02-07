83572d4fb5f81935ab188394031b1aa5c3269a6b
corman <progettocorman@gmail.com>
2018-02-07 15:25:51 +0100

    [Giovanni] sistemazione

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -21,3 +21,9 @@
   <script src="/assets/app_bs3.js"></script>
   <?php $id = session('id'); ?>
 </head>
+<?php
+    $mioid = session('id');
+    if($mioid==""){
+      return redirect('welcome.blade.php');  
+    }
+ ?>
