4e35cf7ff35cf26633b9a27a02624a2898c474f4
progettocorman <christian.angelelli@live.it>
2018-02-08 13:34:12 +0100

    [Christian] Follow e follower

--- resources/views/profile_bar.blade.php
+++ resources/views/profile_bar.blade.php
@@ -6,8 +6,13 @@
     $last_name = $query->last_name;
     $affiliation = $query->affiliation;
     echo $query->user_image;
+    $user_follow = \App\Http\Controllers\UserController::getFollow($id);
+    $user_follower = \App\Http\Controllers\UserController::getFollower($id);
 
-?>" style="width:48px;height:48px;"></p>
+?> " style="width:48px;height:48px;"></p>
 <p>{{$name}}</p>
 <p>{{$last_name}}</p>
 <p>{{$affiliation}}</p>
+<br>
+<p><a class="_t98z6" href="followers"> Follower: <span class="_fd86t" title="360">{{$user_follower}}</span></a></p>
+<p><a class="_t98z6" href="follow"> Follower: <span class="_fd86t" title="360">{{$user_follow}}</span></a></p>
