006c55467cc6fff78d2e44d76c8039e19ae1219d
Enrico <progettocorman@gmail.com>
2018-02-13 08:55:09 +0100

    [Enrico] miglioramento grafica

--- resources/views/information_profile.blade.php
+++ resources/views/information_profile.blade.php
@@ -11,26 +11,26 @@
 <div class="_b0acm">
  <div class="_qdmzb">
    <div class="_62ai2">
-<p><img src="/profile_images/{{$user->user_image}}" style="width:20%;height:20%; -moz-border-radius: 180px; -webkit-border-radius:180px; border-radius:180px; float:left; margin-right: 5%;"></p>
+<p><img src="/profile_images/{{$user->user_image}}" style="width:15%;height:15%; -moz-border-radius: 180px; -webkit-border-radius:180px; border-radius:180px; float:left; margin-right: 5%;"></p>
 </div>
 </div>
 </div>
 
 <section class="_o6mpc">
-<div class="_ienqf">
-<h1>{{$user->name}} {{$user->last_name}} @if($id ==session('id'))<button class="btn btn-primary" type="button" onClick="location.href='settingaccount'">Settings</button> </h1>@endif
-<h2>{{$user->affiliation}}</h2>
+<div class="_t98z6">
+<h3>{{$user->name}} {{$user->last_name}} @if($id ==session('id'))<button class="btn btn-primary" type="button" onClick="location.href='settingaccount'">Settings</button> </h3>@endif
+<h5>{{$user->affiliation}}</h5>
 </div>
 <br>
 <ul class="_h9luf">
   <div class="_bnq48">
     <a class="_t98z6" href="javascript:;" onclick="window.open('/followers', 'titolo', 'width=400, height=200, resizable, status, scrollbars=1, location');">
-          Follower: <span class="_fd86t" title="360">{{$user_follower}}</span>
+          <font size="2px">Follower: </font><span class="_fd86t" title="360"><font size="2px">{{$user_follower}}</font></span>
     </a>
  </div>
   <div class="_bnq48">
     <a class="_t98z6" href="javascript:;" onclick="window.open('/follows', 'titolo', 'width=400, height=200, resizable, status, scrollbars=1, location');">
-          Followed: <span class="_fd86t" title="360">{{$user_follow}}</span>
+          <font size="2px">Followed:</font> <span class="_fd86t" title="360"><font size="2px">{{$user_follow}}</font></span>
     </a>
   </div>
 </ul>
